e777c0e73a38ed34a21702b03d4468e3109281a3
jyneeweenie <jynemarie1@gmail.com>
2024-12-10 12:34:50 +0300

    done

---
+++ Activity-3/main.py
@@ -0,0 +1,11 @@
+def test(list):
+        result = {}
+        for item in list:
+                result[item[0]]
+item[1:]
+        return result
+students = [[1, 'Jean Castro', 'V'], [2, 'Lula Powell', 'V'], [3, 'Brian Howell', 'VI'], [4, 'Lynne Foster', 'VI'], [5, 'Zachary Simon', 'VII']]
+print("\nOriginal lists of lists: ")
+print("students")
+print("\nConverted lists to a dictionary: ")
+print(test(students))
