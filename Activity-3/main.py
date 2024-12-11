61e646a1f9ab8793224f4e451e9b1b3b38c62994
jyneeweenie <jynemarie1@gmail.com>
2024-12-11 10:09:26 +0300

    Corrections have been made

--- Activity-3/main.py
+++ Activity-3/main.py
@@ -1,8 +1,7 @@
 def test(list):
         result = {}
         for item in list:
-                result[item[0]]
-item[1:]
+                result[item[0]] = item[1:]
         return result
 students = [[1, 'Jean Castro', 'V'], [2, 'Lula Powell', 'V'], [3, 'Brian Howell', 'VI'], [4, 'Lynne Foster', 'VI'], [5, 'Zachary Simon', 'VII']]
 print("\nOriginal lists of lists: ")
